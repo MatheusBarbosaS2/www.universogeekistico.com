b8f562e85ec4923836f470c043058743b9dd59a7
MatheusBarbosaS2 <106640087+MatheusBarbosaS2@users.noreply.github.com>
2024-02-05 02:20:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     const newsContainer = document.getElementById("news-container");
 
+    // Função para truncar texto
+    function truncateText(text, maxLength) {
+        return text.length > maxLength ? text.slice(0, maxLength) + '...' : text;
+    }
+
     // Função para obter dinamicamente o conteúdo da notícia
     async function fetchNewsData(newsUrl) {
         try {
@@ -11,7 +16,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
             // Extrai informações do documento da notícia
             const title = newsDoc.querySelector("h2").textContent;
-            const description = newsDoc.querySelector("p").textContent;
+            const descriptionElement = newsDoc.querySelector("p");
+            const description = truncateText(descriptionElement.textContent, 150); // Limite para 150 caracteres
             const thumbnail = newsDoc.querySelector("img").getAttribute("src");
             const fullDescriptionUrl = newsUrl;  // Mantém a URL da notícia original
             const views = parseInt(newsDoc.querySelector("p:last-of-type").textContent.match(/\d+/)[0]);
@@ -31,12 +37,10 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     // Chama a função para a notícia1.html
-
-    fetchNewsData("noticia2.html");
     fetchNewsData("noticia1.html");
 
-    // Pode adicionar mais notícias chamando a função para outros arquivos, por exemplo:
-    // fetchNewsData("noticia2.html");
+    // Adicione mais notícias chamando a função para outros arquivos, por exemplo:
+    fetchNewsData("noticia2.html");
     // fetchNewsData("noticia3.html");
     // ...
 
