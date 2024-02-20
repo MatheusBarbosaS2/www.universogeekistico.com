93470c8bc6d1b3d5360d8cf55ef6cc3ddd72bead
MatheusBarbosaS2 <106640087+MatheusBarbosaS2@users.noreply.github.com>
2024-02-19 21:29:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,21 @@ window.addEventListener("scroll", function() {
     }
     lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Para o Firefox
 }, false);
+
+// Função para limitar o texto a três linhas e adicionar os três pontos
+postTexts.forEach(postText => {
+    const lineHeight = parseFloat(window.getComputedStyle(postText).lineHeight); // Obtém a altura de uma linha
+    const maxLines = 3; // Define o número máximo de linhas
+    const maxHeight = lineHeight * maxLines; // Calcula a altura máxima baseada no número máximo de linhas
+    
+    // Verifica se a altura do texto é maior que a altura máxima permitida
+    if (postText.scrollHeight > maxHeight) {
+        // Define a altura máxima do texto
+        postText.style.maxHeight = maxHeight + 'px';
+        
+        // Adiciona os três pontos no final do texto
+        const originalText = postText.textContent.trim(); // Obtém o texto original sem espaços extras no início e no fim
+        const slicedText = originalText.slice(0, originalText.lastIndexOf(' ', Math.floor((maxHeight / lineHeight) * 0.9))); // Corta o texto até o último espaço antes de atingir a altura máxima
+        postText.textContent = slicedText.trim() + '...'; // Adiciona os três pontos ao final do texto cortado
+    }
+});
